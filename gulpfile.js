133c752d7f6e924b1587f1d3e60e73a5289dd22d
mrsCandyBar <candicekbar@gmail.com>
2017-06-16 19:09:29 +0200

    Add Sass and ESNext compilers to grunt and reordered file structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,33 @@
-var gulp = require('gulp');
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
  
 var paths = {
-  scripts: [
-    'assets/*.js'
-    ],
-  sass: 'assets/*.css'
+  scripts: 'build/js/*.js',
+  sass: 'build/sass/*.scss'
 };
 
 gulp.task('rerun_script', function() {});
-gulp.task('rerun_CSS', function() {});
- 
-// Rerun the task when a file changes 
-gulp.task('watch', function() {
-  gulp.watch(paths.scripts, ['rerun_script']);
-  gulp.watch(paths.sass, ['rerun_CSS']);
+
+gulp.task('runBabel', () => {
+  return gulp.src(paths.scripts)
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('assets/js'));
+})
+
+gulp.task('sass', function() {
+  return gulp.src(paths.sass)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('assets/css'));
 });
  
-// The default task (called when you run `gulp` from cli) 
-gulp.task('default', ['watch']);
+gulp.task('watch', function() {
+  gulp.watch(paths.scripts, ['rerun_script']);
+  gulp.watch(paths.sass, ['sass']);
+});
+ 
+gulp.task('default', ['runBabel', 'sass', 'watch']);
